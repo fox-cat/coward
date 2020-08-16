372a545b85f5641ba7814269658039b2e0cc95c2
MikuroXina <ryosukadnak@gmail.com>
2020-08-17 04:08:37 +0900

    :art: Remove infferable annotations

--- src/Events.ts
+++ src/Events.ts
@@ -28,24 +28,16 @@ export class Events {
   readonly ready = new Emitter();
 
   /** Fired when a channel is created. */
-  readonly channelCreate: Emitter<{ channel: Channel }> = new Emitter<
-    { channel: Channel }
-  >();
+  readonly channelCreate = new Emitter<{ channel: Channel }>();
 
   /** Fired when a channel is updated. */
-  readonly channelUpdate: Emitter<{ channel: Channel }> = new Emitter<
-    { channel: Channel }
-  >();
+  readonly channelUpdate = new Emitter<{ channel: Channel }>();
 
   /** Fired when a channel is deleted. */
-  readonly channelDelete: Emitter<{ channel: Channel }> = new Emitter<
-    { channel: Channel }
-  >();
+  readonly channelDelete = new Emitter<{ channel: Channel }>();
 
   /** Fired when a message is pinned or unpinned in a text channel. This is not fired when a pinned message is deleted. */
-  readonly channelPinsUpdate: Emitter<{ channel: Channel }> = new Emitter<
-    { channel: Channel }
-  >();
+  readonly channelPinsUpdate = new Emitter<{ channel: Channel }>();
 
   /**
    * Fired when
@@ -53,9 +45,7 @@ export class Events {
    * - A guild becomes available to the client.
    * - The client joins a guild.
    */
-  readonly guildCreate: Emitter<{ guild: Guild }> = new Emitter<
-    { guild: Guild }
-  >();
+  readonly guildCreate = new Emitter<{ guild: Guild }>();
 
   /** Fired when a guild is updated. */
   readonly guildUpdate = new Emitter<{ guild: Guild }>();
@@ -65,93 +55,69 @@ export class Events {
    * - The client leaves or is removed from a guild.
    * - A guild becomes unavailable.
    */
-  readonly guildDelete: Emitter<{ guild: Guild }> = new Emitter<
-    { guild: Guild }
-  >();
+  readonly guildDelete = new Emitter<{ guild: Guild }>();
 
   /** Fired when a user is banned from a guild. */
-  readonly guildBanAdd: Emitter<{ guild: Guild; user: User }> = new Emitter<
-    { guild: Guild; user: User }
-  >();
+  readonly guildBanAdd = new Emitter<{ guild: Guild; user: User }>();
 
   /** Fired when a user is unbanned from a guild. */
-  readonly guildBanRemove: Emitter<{ guild: Guild; user: User }> = new Emitter<
-    { guild: Guild; user: User }
-  >();
+  readonly guildBanRemove = new Emitter<{ guild: Guild; user: User }>();
 
   /** Fired when a guild's emojis have been updated. */
-  readonly guildEmojisUpdate: Emitter<
+  readonly guildEmojisUpdate = new Emitter<
     { guild: Guild; emojis: Array<GuildEmoji> }
-  > = new Emitter<{ guild: Guild; emojis: Array<GuildEmoji> }>();
-
-  /** Fired when a guild's integrations are updated. */
-  readonly guildIntegrationsUpdate: Emitter<{ guild: Guild }> = new Emitter<
-    { guild: Guild }
   >();
 
+  /** Fired when a guild's integrations are updated. */
+  readonly guildIntegrationsUpdate = new Emitter<{ guild: Guild }>();
+
   /** Fired when a new user joins a guild. */
-  readonly guildMemberAdd: Emitter<{ guild: Guild; member: GuildMember }> =
-    new Emitter<{ guild: Guild; member: GuildMember }>();
+  readonly guildMemberAdd = new Emitter<
+    { guild: Guild; member: GuildMember }
+  >();
 
   /** Fired when a user leaves or is removed from a guild. */
-  readonly guildMemberRemove: Emitter<{ guild: Guild; member: GuildMember }> =
-    new Emitter<{ guild: Guild; member: GuildMember }>();
+  readonly guildMemberRemove = new Emitter<
+    { guild: Guild; member: GuildMember }
+  >();
 
   /** Fired when a guild member is updated. */
   //TODO: Is there a better way of doing this? :v
-  readonly guildMemberUpdate: Emitter<
-    { guild: Guild; member: GuildMember; oldMember: GuildMember }
-  > = new Emitter<
+  readonly guildMemberUpdate = new Emitter<
     { guild: Guild; member: GuildMember; oldMember: GuildMember }
   >();
 
   //TODO: EmitterGuildMemberChunk: https://discord.com/developers/docs/topics/gateway#guild-members-chunk
 
   /** Fired when a guild role is created. */
-  readonly guildRoleCreate: Emitter<{ guild: Guild; role: Role }> = new Emitter<
-    { guild: Guild; role: Role }
-  >();
+  readonly guildRoleCreate = new Emitter<{ guild: Guild; role: Role }>();
 
   /** Fired when a guild role is updated. */
-  readonly guildRoleUpdate: Emitter<{ guild: Guild; role: Role }> = new Emitter<
-    { guild: Guild; role: Role }
-  >();
+  readonly guildRoleUpdate = new Emitter<{ guild: Guild; role: Role }>();
 
   /** Fired when a guild role is deleted. */
-  readonly guildRoleDelete: Emitter<{ guild: Guild; role: Role }> = new Emitter<
-    { guild: Guild; role: Role }
-  >();
+  readonly guildRoleDelete = new Emitter<{ guild: Guild; role: Role }>();
 
   // TODO: Invites (see https://discord.com/developers/docs/topics/gateway#invites)
 
   /** Fired when a message is created. */
-  readonly messageCreate: Emitter<{ message: Message }> = new Emitter<
-    { message: Message }
-  >();
+  readonly messageCreate = new Emitter<{ message: Message }>();
 
   /** Fired when a message is updated. */
-  readonly messageUpdate: Emitter<{ message: Message }> = new Emitter<
-    { message: Message }
-  >();
+  readonly messageUpdate = new Emitter<{ message: Message }>();
 
   /** Fired when a message is deleted. */
-  readonly messageDelete: Emitter<{ messageID: string; channelID: string }> =
-    new Emitter<{ messageID: string; channelID: string }>();
+  readonly messageDelete = new Emitter<
+    { messageID: string; channelID: string }
+  >();
 
   /** Fired when messages are deleted in bulk. */
-  readonly messageDeleteBulk: Emitter<
+  readonly messageDeleteBulk = new Emitter<
     { messageIDs: Array<string>; channelID: string }
-  > = new Emitter<{ messageIDs: Array<string>; channelID: string }>();
+  >();
 
   /** TODO: Fired when a reaction is added to a message. */
-  readonly messageReactionAdd: Emitter<
-    {
-      user: User;
-      channel: Channel;
-      emoji: ReactionStandardEmoji | ReactionCustomEmoji;
-      messageID: string;
-    }
-  > = new Emitter<
+  readonly messageReactionAdd = new Emitter<
     {
       user: User;
       channel: Channel;
@@ -161,14 +127,7 @@ export class Events {
   >();
 
   /** TODO: Fired when a reaction is removed from a message. */
-  readonly messageReactionRemove: Emitter<
-    {
-      user: User;
-      channel: Channel;
-      emoji: ReactionStandardEmoji | ReactionCustomEmoji;
-      messageID: string;
-    }
-  > = new Emitter<
+  readonly messageReactionRemove = new Emitter<
     {
       user: User;
       channel: Channel;
@@ -178,16 +137,16 @@ export class Events {
   >();
 
   /** TODO: Fired when a user removes all reactions from a message. */
-  readonly messageReactionRemoveAll: Emitter<
+  readonly messageReactionRemoveAll = new Emitter<
     { channel: Channel; messageID: string }
-  > = new Emitter<{ channel: Channel; messageID: string }>();
+  >();
 
   // TODO: Presence ...https://discord.com/developers/docs/topics/gateway#presence-update
 
   /** Fired when a user starts typing in a channel. */
-  readonly typingStart: Emitter<
+  readonly typingStart = new Emitter<
     { channel: Channel; userID: string; timestamp: number }
-  > = new Emitter<{ channel: Channel; userID: string; timestamp: number }>();
+  >();
 
   // TODO: Voice ...https://discord.com/developers/docs/topics/gateway#voice
 }
